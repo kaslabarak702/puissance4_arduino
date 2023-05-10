217aa9d021a559d7fd99491a67de77ea74e5230b
kaslabarak702 <101200976+kaslabarak702@users.noreply.github.com>
2023-05-10 15:17:33 +0200

    nettoyage classe pion

--- puissance4/puissance4.ino
+++ puissance4/puissance4.ino
@@ -1,6 +1,5 @@
 #include <Gamebuino-Meta.h>
 
-// Déclaration de la classe Pion
 class Pion {
   private:
     int coordonnee_i; // Coordonnée i du pion
@@ -8,35 +7,29 @@ class Pion {
     int numero_joueur; // Numéro du joueur associé au pion
     
   public:
-    // Constructeur
     Pion(int i, int j, int joueur) {
       coordonnee_i = i;
       coordonnee_j = j;
       numero_joueur = joueur;
     }
     
-    // Méthode pour obtenir la coordonnée i du pion
     int getCoordonneeI() {
       return coordonnee_i;
     }
     
-    // Méthode pour obtenir la coordonnée j du pion
     int getCoordonneeJ() {
       return coordonnee_j;
     }
     
-    // Méthode pour définir les coordonnées i et j du pion
     void setCoordonnees(int i, int j) {
       coordonnee_i = i;
       coordonnee_j = j;
     }
     
-    // Méthode pour obtenir le numéro du joueur associé au pion
     int getNumeroJoueur() {
       return numero_joueur;
     }
     
-    // Méthode pour définir le numéro du joueur associé au pion
     void setNumeroJoueur(int joueur) {
       numero_joueur = joueur;
     }
@@ -49,9 +42,9 @@ int grille[GRILLE_TAILLEX][GRILLE_TAILLEY] = {{1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1
 
 
 float ligne_hauteur = 2;
-float ligne_largeur = 70;
+float ligne_largeur = 50;
 
-float colonne_hauteur = 60;
+float colonne_hauteur = 5;
 float colonne_largeur = 2;
 
 void setup() {
@@ -60,12 +53,9 @@ void setup() {
 }
 
 void loop() {
-//tant que (!quelqunAgagné)
-//Verif cases jouable (colonne vide)
-//demander selection(aux 2 jouers alternativement
-//fin du tour inverser la variable booleene
+//tant que gagner == false
+//Verif cases jouable (colonne vide, 
   
-  // put your main code here, to run repeatedly:
   while (!gb.update());
   gb.display.clear();
 
@@ -74,13 +64,6 @@ void loop() {
   for (int x = 0; x < GRILLE_TAILLEX +1; x += 1){
     
     int displayY = x * (colonne_largeur + 6) + 1;
-    
-    gb.display.fillRect(4, displayY, ligne_largeur, ligne_hauteur);
-  }
-  for (int x = 0; x < GRILLE_TAILLEY +1; x += 1){
-    
-    int displayX = x * (ligne_hauteur + 6) + 1;
-    
-    gb.display.fillRect(displayX, 3, colonne_largeur, colonne_hauteur);
+    gb.display.fillRect(ligne_largeur, displayY, ligne_largeur, ligne_hauteur);
   }
 }
